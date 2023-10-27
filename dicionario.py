99ab24e958f6af84434967286519905be487de86
Rogério Alcantara <rogerio.alunos@cfp127aluno.local>
2023-10-26 21:48:44 -0300

    dicionario_python

---
+++ dicionario.py
@@ -0,0 +1,22 @@
+agenda = {'nome':'Sarah', 'phone':'1234-5678', 'email':'Sarah@gmail.com', 'idade':'65'}
+"""print(agenda['idade'])
+print(agenda.keys()) #lista as chaves
+print(agenda.values()) #lista os valores"""
+
+for dicionario in agenda.keys():
+    print(agenda[dicionario])
+
+
+
+"""Tuplas e List
+
+Tuplas não podem ser alteradas, se forem alteradas, cria-se uma nova tupla
+são imutaveis
+
+list = []
+tuplas = ()
+dicionario = {}
+
+dicionario = {'boy': 'muchacho'}
+
+dic = {'boy': 'muchacho', 'vetar': 'se opor'}"""
